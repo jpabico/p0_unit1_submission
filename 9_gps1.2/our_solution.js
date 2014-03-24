95a896f733d1584d0cc0c3420daf11f1a59e802a
jpabico <jbpabico@gmail.com>
2014-03-24 10:28:59 -0700

    Update our_solution.js

--- 9_gps1.2/our_solution.js
+++ 9_gps1.2/our_solution.js
@@ -1,24 +1,39 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Jon Pabico
+//  2. Jake Persing
 
 
-// 1. "YOU SIGNED... WHO?!"
+//  1. "YOU SIGNED... WHO?!"
 
+// define object constructor
+  function Client(name, age, quote){
+    this.name = name,
+    this.age = age,
+    this.quote = quote
+  }
 
+// created 2 new instances of the Client object
+var beyonce = new Client("Beyonce", 50, "If you like it, you'd better put a ring on it.");
+var karlUrban = new Client("Karl Urban", 50, "All she left me was my bones.");
 
 
 // 2. "Here they Come!"
 
+// created 3 more new instances of the Client object based on given info
+var adamSandler = new Client("Adam Sandler", 47, "That's your home! Are you too good for your home?!");
+var kristenBell = new Client("Kristen Bell", 33, "Do you wanna build a snowman?");
+var jimCarrey = new Client("Jim Carrey", 52, "...So you're telling me there's a chance? YEAH!");
 
 
+//    3. "TIME IS MONEY!"
 
+// YOUR CODE HERE!
 
-// 3. "TIME IS MONEY!"
-
-//YOUR CODE HERE!
+// none of our code was refactored because we started off using the object constructor
+// Tyler told us that it was anticipated that we were going 
+// to use object literal notation, not the object constructor
 
 var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
 shooterMcGavin.constructor === Client;
@@ -26,12 +41,29 @@ shooterMcGavin.age === 48;
 shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.";
 
 
-
-
-
 // 4. "SHOW 'EM OFF!"
 
 
+// defined display function
+Client.prototype.display = function() {
+  console.log("Introducing: " + this.name);
+  console.log("This person is: " + this.age);
+  console.log("Their quote is: " + this.quote);
+}
+
+// an array of celebrities 
+var celebArray = [beyonce, karlUrban, adamSandler, kristenBell, jimCarrey, shooterMcGavin];
 
 
-// ** BONUS **
+//       ** BONUS **
+
+// modified display function to be called by the for loop below
+
+// for loop to call display function on each element of celebArray
+for(var i = 0;i < celebArray.length;i++){
+  celebArray[i].display();
+}
+
+
+
+
