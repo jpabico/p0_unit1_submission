32a34b7f79a966ab7c8dfa96f8f98e1666336b06
jpabico <jbpabico@gmail.com>
2014-03-24 00:27:21 -0700

    entered pseudocode and initial solution

--- 1_calculate_array_total/my_solution.rb
+++ 1_calculate_array_total/my_solution.rb
@@ -4,18 +4,29 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with: Kevin Kang.
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# define a method called "total"
+# it will take one argument (an array)
+# it will find the sum of the numbers in the array
+
+# define a method called "sentence_maker"
+# it will take one argument (an array)
+# it will join all elements and capitalize the first word
+#   and add a period at the end
 
 
 # 2. Initial Solution
 
+def total(array)
+  array.inject(0) {|memo,n| memo+n}
+end
 
+def sentence_maker(array)
+  array.join(" ").capitalize! + "."
+end
 
 
 # 3. Refactored Solution
