eaa26e6c3583dfd1d0b251ef30383383812967d3
Jon Pabico <jbpabico@gmail.com>
2014-03-25 20:55:09 -0700

    pseudocode and initial solution only

--- 4_median_solo_challenge/my_solution.rb
+++ 4_median_solo_challenge/my_solution.rb
@@ -8,15 +8,36 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
-
+# define method called "median" that takes one parameter (myArray)
+# find size of array
+# sort array by comparing value of each element (switch if out of order)
+# calculate median of array with odd-number length (middle number)
+# calculate median of array with even-number length (average of 2 middles)
 
 # 2. Initial Solution
 
+def median(myArray)
+	n = myArray.length
 
+	# sort array
+	for i in 0...(n-1)
+		for j in 1..(n-1)
+			if myArray[j-1] > myArray[j]
+				temp = myArray[j]
+				myArray[j] = myArray[j-1]
+				myArray[j-1] = temp
+			end
+		end
+	end
 
+	# find median
+	if (n%2==0)
+		med = (myArray[(n/2)-1] + myArray[n/2]).to_f/2
+		return med
+	else
+		return myArray[(n-1)/2]
+	end
+end
 
 # 3. Refactored Solution
 
