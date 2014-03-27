68bea3b57332a195f7e08dc28379b69001ead64b
Jon Pabico <jbpabico@gmail.com>
2014-03-26 22:23:13 -0700

    initial solution and refactored solution

--- 4_median_solo_challenge/my_solution.rb
+++ 4_median_solo_challenge/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
@@ -16,31 +16,40 @@
 
 # 2. Initial Solution
 
-def median(myArray)
-	n = myArray.length
+# def median(myArray)
+# 	n = myArray.length
 
-	# sort array
-	for i in 0...(n-1)
-		for j in 1..(n-1)
-			if myArray[j-1] > myArray[j]
-				temp = myArray[j]
-				myArray[j] = myArray[j-1]
-				myArray[j-1] = temp
-			end
-		end
-	end
+# 	# sort array
+# 	for i in 0...(n-1)
+# 		for j in 1..(n-1)
+# 			if myArray[j-1] > myArray[j]
+# 				temp = myArray[j]
+# 				myArray[j] = myArray[j-1]
+# 				myArray[j-1] = temp
+# 			end
+# 		end
+# 	end
 
-	# find median
-	if (n%2==0)
-		med = (myArray[(n/2)-1] + myArray[n/2]).to_f/2
-		return med
-	else
-		return myArray[(n-1)/2]
-	end
-end
+# 	# find median
+# 	if (n%2==0)
+# 		med = (myArray[(n/2)-1] + myArray[n/2]).to_f/2
+# 		return med
+# 	else
+# 		return myArray[(n-1)/2]
+# 	end
+# end
 
 # 3. Refactored Solution
 
+def median(myArray)
+	n = myArray.length
 
+	myArray.sort!
+
+	if (n%2==0)
+		return (myArray[(n/2)-1] + myArray[n/2]).to_f/2
+	end
+		return myArray[(n-1)/2]
+end
 
 # 4. Reflection 
