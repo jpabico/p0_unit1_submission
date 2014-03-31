e32c69d2689f3cef34a2bd1789e41630838b09a9
Jon Pabico <jbpabico@gmail.com>
2014-03-30 20:07:54 -0700

    pseudocode and initial solution

--- 10_BONUS_mode/my_solution.rb
+++ 10_BONUS_mode/my_solution.rb
@@ -9,18 +9,46 @@
 # 1. Pseudocode
 
 # What is the input?
+# array of numbers and strings
 # What is the output? (i.e. What should the code return?)
+# An array of the most frequent values from our input
 # What are the steps needed to solve the problem?
+# define a method that takes an array
+# scan each object in the array and create a counter that tallies 
+#    how many times each object appears
+# Array ---> Scan [0] ---> check if new array has this object 
+#    already --> if yes, increase a method counter by one, 
+#    if no, create a count for that object in our new array (object_count)
+
+# # 2. Initial Solution
+
+def mode(array)
+    freqs = Hash.new(0)
+    array.each { |element| freqs[element] +=1 }
+    
+    freqs = freqs.sort_by { |x,y| y }
+    freqs.reverse!
+    freqs.keep_if { |x,y| y == freqs[0][1] }
+    freqs.flatten!
+    filter = []
+    for i in 0...freqs.length
+    	if i%2==0
+    	  filter << freqs[i]
+    	end
+    end
+    return filter.sort
+
+end
 
 
-# 2. Initial Solution
-nums = [2, 4, 6, 7, 8, 7, 9, 0, 2, 7, 5, 7, 3, 7, 1, 72]
-freqs = Hash.new(0)
-nums.each { |num| freqs[num] += 1 }
-freqs = freqs.sort_by {|x,y| y }
-freqs.reverse!
-puts freqs[0][0]
-freqs.each {|num, freq| puts "The number " + num.to_s + " occurs " + freq.to_s + " time(s)"}
+
+# nums = [2, 4, 6, 7, 8, 7, 9, 0, 2, 7, 5, 7, 3, 7, 1, 72]
+# freqs = Hash.new(0)
+# nums.each { |num| freqs[num] += 1 }
+# freqs = freqs.sort_by {|x,y| y }
+# freqs.reverse!
+# puts freqs[0][0]
+# freqs.each {|num, freq| puts "The number " + num.to_s + " occurs " + freq.to_s + " time(s)"}
 
 
 
@@ -29,3 +57,6 @@ freqs.each {|num, freq| puts "The number " + num.to_s + " occurs " + freq.to_s +
 
 
 # 4. Reflection 
+
+
+
